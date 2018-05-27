4b99493814e4cce8a9e8fdc1809a99cf4ee00aed
darc0502 <kobataka61116@yahoo.co.jp>
2018-05-28 00:02:49 +0900

    課題及び発展課題
    よろしくお願いします。

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+
+
+
+	// Use this for initialization
+	void Start () {
+
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+	}
+
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.tag == "CarTag" || other.gameObject.tag == "ConeTag"){
+			Destroy (other.gameObject);
+		}
+		if(other.gameObject.tag == "GoalTag"){
+			Destroy (other.gameObject);
+		}
+		if (other.gameObject.tag == "CoinTag") {
+			Destroy (other.gameObject);
+		}
+	}
+}
